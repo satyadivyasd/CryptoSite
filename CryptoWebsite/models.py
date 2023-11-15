280223e20c97af2acf46ad64b133b0b7b8e34545
divya <ase.team25@gmail.com>
2023-11-15 01:29:46 -0500

    Home page

--- CryptoWebsite/models.py
+++ CryptoWebsite/models.py
@@ -1,18 +1,33 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Currency(models.Model):
-    name=models.CharField(max_length=100)
-    code=models.CharField(max_length=100)
-    def __str__(self):
-        return self.code
-class StockData(models.Model):
-    stock = models.IntegerField()
-    stock_rate = models.FloatField()
-    date = models.DateField()
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
 
     def __str__(self):
-        return  self.date.strftime('%B %Y')
+        return self.code
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+class StockData(models.Model):
+    name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=10)
+    timestamp = models.DateTimeField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    # Add other fields as needed
+# models.py
 
 
--- IADSProject/urls.py
+++ IADSProject/urls.py
@@ -14,18 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from CryptoWebsite import views
+from CryptoWebsite.views import  FetchStockDataView
 
 urlpatterns = [
     # path('', admin.site.urls),  # Assuming 'home' is the name of your view function for the root path
     path('admin/', admin.site.urls),
     path("",views.home,name='home'),
     path('convert_currency/<int:amount>/<str:from_currency>/<str:to_currency>/', views.convert_currency, name='convert_currency'),
-    path('monthly_stock_data',views.monthly_stock_data,name='monthly_stock_data'),
+    # path('monthly_stock_data',views.monthly_stock_data,name='monthly_stock_data'),
     path('chart',views.chart,name='chart'),
-    path('data',views.crypto_data,name='data')
+    path('data',views.crypto_data,name='data'),
+    path('register/',views.register,name='register'),
+    path('login/',views.user_login,name='login'),
+    path('myaccount/', views.myaccount, name='myaccount'),
+    path('generate_graph',views.generate_graph,name='generate_graph'),
+    path('fetch_stock_data/', FetchStockDataView.as_view(), name='fetch_stock_data'),
 
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
